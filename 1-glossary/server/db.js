ba5192654ab568533e966ebb7bfd5d14486bd0ed
Noah Beito <noah.beito@gmail.com>
2023-04-26 15:32:07 -0700

    add a save and getAll model methods that query the db

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,13 +1,37 @@
 const mongoose = require("mongoose");
 
+console.log('DB NAME', process.env.DB_NAME);
 // 1. Use mongoose to establish a connection to MongoDB
-mongoose.connect('mongodb://127.0.0.1:3000/glossary');
+mongoose.connect(`mongodb://localhost/${process.env.DB_NAME}`);
 // 2. Set up any schema and models needed by the app
-const schema = new mongoose.Schema({
+const schema = mongoose.Schema({
   word: {type: String, unique: true},
   definition: String
 });
 
 const Entry = mongoose.model('Entry', schema);
+
+
+const save = (word, definition) => {
+  const filter = { word: word};
+  const update = { definition: definition};
+
+  return Entry.findOneAndUpdate(filter, update, {
+    new: true,
+    upsert: true
+  })
+  .then(() => {
+    return getAll()
+  });
+};
+
+const getAll = () => {
+  return Entry.find({}, 'word definition')
+    .then((response) => {
+      return response;
+    });
+}
 // 3. Export the models
+module.exports.save = save;
+module.exports.getAll = getAll;
 // 4. Import the models into any modules that need them
