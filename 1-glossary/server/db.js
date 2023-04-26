8b882ca7cf47c7e359034b360a25c993a382f590
Noah Beito <noah.beito@gmail.com>
2023-04-26 14:08:31 -0700

    connection to mongodb, schema and model added

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,6 +1,13 @@
 const mongoose = require("mongoose");
 
 // 1. Use mongoose to establish a connection to MongoDB
+mongoose.connect('mongodb://127.0.0.1:3000/glossary');
 // 2. Set up any schema and models needed by the app
+const schema = new mongoose.Schema({
+  word: {type: String, unique: true},
+  definition: String
+});
+
+const Entry = mongoose.model('Entry', schema);
 // 3. Export the models
 // 4. Import the models into any modules that need them
