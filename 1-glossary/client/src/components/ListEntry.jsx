f86872d2510dfe41152fa20023a0ed7d3f35d5d2
Noah Beito <noah.beito@gmail.com>
2023-04-26 16:58:56 -0700

    delete button complete

--- 1-glossary/client/src/components/ListEntry.jsx
+++ 1-glossary/client/src/components/ListEntry.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import axios from 'axios';
 
-const ListEntry = ({entry}) => (
-  <div>
-    <div>{entry.word}</div>
-    <div>{entry.definition}</div>
-  </div>
-)
+const ListEntry = ({entry, renderList}) => {
+
+  const deleteHandler = (word) => {
+    axios.delete('/glossary', {
+      data: {
+        word: word
+      }
+    })
+      .then(() => {
+        return axios.get('/glossary')
+      })
+      .then((response) => {
+        renderList(response.data)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+  return(
+    <div>
+      <div>{entry.word}</div>
+      <div>{entry.definition}</div>
+      <button onClick={() => {deleteHandler(entry.word)}}>Delete</button>
+    </div>
+  )
+}
+
 
 
 export default ListEntry;
