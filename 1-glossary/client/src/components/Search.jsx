dd60eab063e89c4bb86e3f0b4b130af0dd737165
Noah Beito <noah.beito@gmail.com>
2023-04-26 14:05:27 -0700

    add Search component in Search.jsx

---
+++ 1-glossary/client/src/components/Search.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+
+const Search = () => (
+  <div>
+    <input type="text" onChange={() => }></input>
+    <button>Search</button>
+  </div>
+);
+
+export default Search;
