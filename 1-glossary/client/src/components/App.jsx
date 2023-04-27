104591ffbce575b6c83784ebaf08d7b9f0999780
Noah Beito <noah.beito@gmail.com>
2023-04-27 10:53:57 -0700

    cleaned up comments

--- 1-glossary/client/src/components/App.jsx
+++ 1-glossary/client/src/components/App.jsx
@@ -10,20 +10,15 @@ const App = () => {
   const [showSearchList, setShowSearchList] = useState(false);
 
   useEffect(() => {
-    // get request to the database
     axios.get('/glossary')
       .then((response) => {
         setList(response.data);
       })
-    // upon response, setList with array of data
   }, []);
 
   const searchHandler = (input) => {
-    // create searchedArray
     let searched = [];
-    // iterate list
     list.forEach((word, index) => {
-      // if current search input matches item in list
       let currWord = word.word.toLowerCase();
       input = input.toLowerCase();
       if (currWord.includes(input)) {
