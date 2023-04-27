863852d51fe5f0b3a2480fa2aa844875d5d7de16
Noah Beito <noah.beito@gmail.com>
2023-04-26 17:37:29 -0700

    edit button in progress. works to edit definition, but if you edit the word it will create a new entry

--- 1-glossary/client/src/components/ListEntry.jsx
+++ 1-glossary/client/src/components/ListEntry.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 const ListEntry = ({entry, renderList}) => {
+  const [edit, setEdit] = useState(false);
+  const [wordEdit, setWordEdit] = useState(entry.word);
+  const [definitionEdit, setDefinitionEdit] = useState(entry.definition);
 
   const deleteHandler = (word) => {
     axios.delete('/glossary', {
@@ -20,13 +23,48 @@ const ListEntry = ({entry, renderList}) => {
       })
   }
 
-  return(
-    <div>
-      <div>{entry.word}</div>
-      <div>{entry.definition}</div>
-      <button onClick={() => {deleteHandler(entry.word)}}>Delete</button>
-    </div>
-  )
+  const handleEdit = (word, definition) => {
+    // axios PUT request
+      // honestly the way we have set up the post request that might work too lets try that first
+
+    axios.post('/glossary', {
+      word: word,
+      definition: definition
+    })
+    .then(() => {
+      return axios.get('/glossary')
+    })
+    .then((response) => {
+      renderList(response.data);
+      setEdit(false);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
+  if (!edit) {
+    return (
+      <div>
+        <div>{entry.word}</div>
+        <div>{entry.definition}</div>
+        <button onClick={() => {deleteHandler(entry.word)}}>Delete</button>
+        <button onClick={() => {setEdit(true)}}>Edit</button>
+      </div>
+    )
+  }
+
+  if (edit) {
+    return (
+      <div>
+        <input type="text" onChange={(e) => {setWordEdit(e.target.value)}}defaultValue={entry.word}></input>
+        <br />
+        <input type="text" onChange={(e) => {setDefinitionEdit(e.target.value)}}defaultValue={entry.definition}></input>
+        <button onClick={() => {handleEdit(wordEdit, definitionEdit)}}>Submit</button>
+        <button onClick={() => {setEdit(false)}}>Cancel</button>
+      </div>
+    )
+  }
 }
 
 
