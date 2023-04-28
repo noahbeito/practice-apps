dd2c9b1b063a2077c54152516de380e6b12ada9d
Noah Beito <noah.beito@gmail.com>
2023-04-28 11:21:42 -0700

    added schema file to create new database

---
+++ 2-checkout/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE checkout;
+
+CREATE DATABASE checkout;
+
+USE checkout;
+
+-- CREATE TABLE IF NOT EXISTS responses (
+--   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   session_id INT NOT NULL UNIQUE,
+--   username VARCHAR(255) NOT NULL,
+--   email VARCHAR(255) NOT NULL,
+--   user_password VARCHAR(255) NOT NULL,
+--   ship_address_line_1 VARCHAR(255) NOT NULL,
+--   ship_address_line_2 VARCHAR(255),
+--   ship_city VARCHAR(255) NOT NULL,
+--   ship_state VARCHAR(255) NOT NULL,
+--   ship_zip VARCHAR(255) NOT NULL,
+--   phone_number VARCHAR(255) NOT NULL,
+--   cc_number INT NOT NULL,
+--   cc_expire_date VARCHAR(255) NOT NULL,
+--   cvv CHAR(5) NOT NULL,
+--   billing_zip VARCHAR(255) NOT NULL
+-- );
+
