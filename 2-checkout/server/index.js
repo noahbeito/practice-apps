8bd0a769191ab71e7ea4954b51cd7eb2ad972f97
Noah Beito <noah.beito@gmail.com>
2023-04-28 15:58:31 -0700

    added routes with psuedocode

--- 2-checkout/server/index.js
+++ 2-checkout/server/index.js
@@ -30,5 +30,32 @@ app.use(express.json());
  *
  */
 
+app.post('/responses', (req, res) => {
+  // invoke save method from models
+  // we want to pass along relevant data
+  // maybe easiest to send data over in an object
+  // and then we can handle that data in the model???? or here
+  // id like to avoid 3 different save methods
+  // jk post will only be for F1 submission
+  // then put requests for F2 and F3
+
+  // then (upon response) send the client
+  // status code 201
+})
+
+app.put('/responses', (req, res) => {
+  // invoke update method from models
+   // it will need session_id and new data to add
+
+  // upon success, send 200 code to client.
+})
+
+app.get('/responses', (req, res) => {
+  // invoke getAll from models
+  // then (upon response) repond to the client with
+  // 200 status code and the response data
+  // ** check formatting of data
+})
+
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
