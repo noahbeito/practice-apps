1c78ec69f0b7b81f94e9514536031980d8be9b90
Noah Beito <noah.beito@gmail.com>
2023-04-29 12:03:59 -0700

    not sure why this is committing server/db.js. no functional changes in that file

--- 2-checkout/client/src/components/F3.jsx
+++ 2-checkout/client/src/components/F3.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const F3 = ({ handleSetView }) => {
-
-  // states for input fields
+  const [cc, setCc] = useState('');
+  const [expDate, setExpDate] = useState('');
+  const [cvv, setCvv] = useState('');
+  const [zip, setZip] = useState('');
 
   const handleClick = () => {
     // send put request with new data and session_id
-    // then
-    handleSetView('Checkout');
+    axios.put('/responses/F3', {
+      cc_number: cc,
+      exp_date: expDate,
+      cvv: cvv,
+      billing_zip: zip
+    })
+    .then(() => {
+      handleSetView('Checkout');
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+
   }
 
   return (
@@ -15,19 +29,19 @@ const F3 = ({ handleSetView }) => {
       <h2>Billing</h2>
       <br />
       <label>Credit Card Number:
-        <input type="text"></input>
+        <input type="text" onChange={(e) => {setCc(e.target.value)}}></input>
       </label>
       <br />
       <label>Expiration Date:
-        <input type="text"></input>
+        <input type="text" onChange={(e) => {setExpDate(e.target.value)}}></input>
       </label>
       <br />
       <label>CVV:
-        <input type="text"></input>
+        <input type="text" onChange={(e) => {setCvv(e.target.value)}}></input>
       </label>
       <br />
       <label>Zipcode:
-        <input type="text"></input>
+        <input type="text" onChange={(e) => {setZip(e.target.value)}}></input>
       </label>
       <br />
       <button onClick={() => {handleClick()}}>Next</button>
