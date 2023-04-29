2ec69ea551c7714dcc531de4b71b06ba15fc3833
Noah Beito <noah.beito@gmail.com>
2023-04-29 10:46:50 -0700

    completed update method for F2

--- 2-checkout/server/models.js
+++ 2-checkout/server/models.js
@@ -3,16 +3,27 @@ const db = require('./db');
 // there will be different arguments passed to save
 // depending on which form is being submitted. may need to adjust
 // the parameters
-const save = (...vals) => {
-  db.queryAsync(
+const save = (session_id, name, email, password) => {
+  return db.queryAsync(
     // save inputs to db
-    'INSERT INTO responses VALUES (.....change this........)'
+    `INSERT INTO responses (session_id, name, email, password) VALUES (?, ?, ?, ?)`, [session_id, name, email, password]
   )
 }
 
-const update = () => {
+const update = (ship1, ship2, city, state, zip, phone_number, session_id) => {
   // find by session_id or id
     // then add data to relevant fields
+  return db.queryAsync(
+    `UPDATE responses
+     SET ship_address_line_1 = ?,
+         ship_address_line_2 = ?,
+         ship_city = ?,
+         ship_state = ?,
+         ship_zip = ?,
+         phone_number = ?
+     WHERE responses.session_id = ?`,
+    [ship1, ship2, city, state, zip, phone_number, session_id]
+  )
 }
 
 const getAll = () => {
@@ -22,3 +33,4 @@ const getAll = () => {
 
 module.exports.save = save;
 module.exports.getAll = getAll;
+module.exports.update = update;
