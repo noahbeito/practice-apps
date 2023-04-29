5fcd0575f41d51c8fea02d8f2e62ac10653e6f1b
Noah Beito <noah.beito@gmail.com>
2023-04-29 11:19:30 -0700

    updated prop

--- 2-checkout/client/src/components/Checkout.jsx
+++ 2-checkout/client/src/components/Checkout.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 
-const Checkout = () => {
+const Checkout = ({ handleSetView }) => {
+
+  // send get request with session_id
+  // upon response render data
+
+  const handleClick = () => {
+    handleSetView('Home');
+  }
+
+  return (
+    <div>
+      <div>Show all input data</div>
+      <button onClick={() => {handleClick()}}>Purchase</button>
+    </div>
+  )
 
 }
 
